1e9db5a84ec84ddab0d1bc6f4bb8a713da22c298
yawnhaaa <yawnhaaa@163.com>
2024-02-28 21:56:43 +0800

    feat: Layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import MainLayout from './components/MainLayout.vue';
 </script>
 
 <template>
-
+  <h1>Hello</h1>
+  <MainLayout />
 </template>
 
 <style scoped>
--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,1 +1,30 @@
-// test
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">首页</RouterLink>
+      <RouterLink to="/material">素材</RouterLink>
+      <RouterLink to="/learn">学习</RouterLink>
+      <RouterLink to="/visual">可视化</RouterLink>
+      <RouterLink to="/issue">发布</RouterLink>
+      <RouterLink to="/my">个人信息</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  <footer>
+    <div class="friend-link">
+      <div>友情链接</div>
+    </div>
+    <div class="copyright">
+      <p>举报邮箱：shuaige@xxx.xx 座机电话：0571-xxxxxxxx</p>
+      <p>&copy 大学反诈中心. All Rights Reserved</p>
+      <p>浙ICP证：2024号xxxxxx号-1</p>
+      <p>浙公网安备110xxxxxxxxxxx号</p>
+    </div>
+  </footer>
+</template>
