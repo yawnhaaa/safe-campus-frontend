affd88a2e3d40d6c7ef03b7a6e0f0a1f5e82da6c
yawnhaaa <yawnhaaa@163.com>
2024-03-09 22:12:27 +0800

    渲染好了图片、视频、音频

---
+++ src/components/MateriaItem.vue
@@ -0,0 +1,67 @@
+<script lang="ts">
+import { defineComponent, onMounted, PropType } from 'vue'
+
+type ItemType = {
+    id: string;
+    src: string;
+    title: string;
+    author: string;
+    date: string;
+    type: string;
+}
+
+export default defineComponent({
+    name: 'MateriaItem',
+    props: {
+        itemList: {
+            type: Object as PropType<ItemType>,
+            required: true
+        }
+    },
+    setup(_) {
+    }
+
+})
+</script>
+
+<template>
+    <div class="materia-item" v-for="item in itemList" :key="item.id" >
+        <img :src="item.src" class="materia-image" />
+        <div class="materia-info">
+            <div class="materia-title">{{ item.title }}</div>
+            <div class="materia-footer">
+                <span class="materia-author">{{ item.author }}</span>
+                <span class="materia-date">{{ item.date }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.materia-item {
+    margin-top: 5px;
+    width: calc(50% - 15px);
+    border: 1px solid gray;
+    border-radius: 5px;
+    margin-left: 10px;
+    cursor: pointer;
+
+    .materia-image {
+        width: calc(100% - 10px);
+        padding: 5px;
+    }
+
+    .materia-info {
+        margin-left: 5px;
+
+        .materia-footer {
+            font-size: 14px;
+            color: gray;
+
+            span {
+                margin-right: 10px;
+            }
+        }
+    }
+}
+</style>
--- src/pages/MaterialPage.vue
+++ src/pages/MaterialPage.vue
@@ -1,11 +1,161 @@
 <script lang="ts">
-import { defineComponent, } from 'vue';
+import { defineComponent, ref, } from 'vue';
+import MenuItem from '../components/MenuItem.vue'
+import MateriaItem from '../components/MateriaItem.vue'
 
 export default defineComponent({
     name: 'MaterialPage',
+    components: {
+        MenuItem,
+        MateriaItem,
+    },
+    setup() {
+        const menuItemList = ref(['图片区', '视频区', '音频区'])
+        const itemIndex = ref('0')
+        const handleIndex = (index: string) => {
+            itemIndex.value = index;
+        }
+
+        // 测试数据
+        const imageList = ref([
+            {
+                id: '0',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '图像0',
+                author: '阿赵',
+                date: '3-7',
+                type: '0',
+            },
+            {
+                id: '1',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '图像1',
+                author: '阿宇',
+                date: '3-8',
+                type: '0',
+            },
+            {
+                id: '2',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '图像2',
+                author: '阿豪',
+                date: '3-0',
+                type: '0',
+            },
+        ])
+        const videoList = ref([
+            {
+                id: '0',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '视频0',
+                author: '阿赵视频',
+                date: '3-7',
+                type: '1',
+            },
+            {
+                id: '1',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '视频1',
+                author: '阿宇视频',
+                date: '3-8',
+                type: '1',
+            },
+            {
+                id: '2',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '视频2',
+                author: '阿豪视频',
+                date: '3-0',
+                type: '1',
+            },
+            {
+                id: '2',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '视频3',
+                author: '阿豪视频',
+                date: '3-11',
+                type: '1',
+            },
+        ])
+        const audioList = ref([
+            {
+                id: '0',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '音频0',
+                author: '阿赵音频',
+                date: '3-7',
+                type: '2',
+            },
+            {
+                id: '1',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '音频1',
+                author: '阿宇音频',
+                date: '3-8',
+                type: '2',
+            },
+            {
+                id: '2',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '音频2',
+                author: '阿豪音频',
+                date: '3-0',
+                type: '2',
+            },
+            {
+                id: '2',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '音频3',
+                author: '阿豪音频',
+                date: '3-11',
+                type: '2',
+            },
+            {
+                id: '2',
+                src: 'src/assets/images/fanzha.jpg',
+                title: '音频4',
+                author: '阿豪音频',
+                date: '3-12',
+                type: '2',
+            },
+        ])
+
+        return {
+            menuItemList,
+            itemIndex,
+            handleIndex,
+            imageList,
+            videoList,
+            audioList,
+        }
+    }
 })
 </script>
 
 <template>
-    <h1>Material Page</h1>
+    <div class="page-contain">
+        <menu-item :menuItemList="menuItemList" @item-selected="handleIndex"/>
+        <template v-if="itemIndex == 0">
+            <div class="image-contain">
+                <materia-item :itemList="imageList" />
+            </div>
+        </template>
+        <template v-else-if="itemIndex == 1">
+            <div class="video-contain">
+                <materia-item :itemList="videoList" />
+            </div>
+        </template>
+        <template v-else>
+            <div class="audio-contain">
+                <materia-item :itemList="audioList" />
+            </div>
+        </template>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.image-contain, .video-contain, .audio-contain {
+    display: flex;
+    flex-wrap: wrap;
+}
+</style>
