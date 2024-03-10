4bfa464e1041c994d4499a2450175f1928aeb56f
yawnhaaa <yawnhaaa@163.com>
2024-03-10 19:01:36 +0800

    素材页面，加油！

--- src/pages/IssuePage.vue
+++ src/pages/IssuePage.vue
@@ -1,12 +1,195 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
+import '../styles/page.scss'
+import MenuItem from '../components/MenuItem.vue'
+import { ElMessageBox, FormInstance, FormRules } from 'element-plus'
+import { UploadFilled } from '@element-plus/icons-vue'
 
 export default defineComponent({
     name: "IssuePage",
+    components: {
+        MenuItem,
+        UploadFilled,
+    },
+
+    setup() {
+        type InfoFormType = {
+            title: string;
+            author: string;
+            date: string;
+            content: string;
+        }
+        type MateriaFormType = {
+            title: string;
+            author: string;
+            date: string;
+            radio: string;
+            content: string;
+        }
+
+        const menuItemList = ref(['资讯上传', '素材上传'])
+        const itemIndex = ref('0')
+        const handleIndex = (index: string) => {
+            itemIndex.value = index
+        }
+
+        // 资讯表单
+        const infoFormRef = ref<FormInstance>()
+        const infoForm = reactive<InfoFormType>({
+            title: '',
+            author: '',
+            date: '',
+            content: '',
+        })
+        const infoFormRules = reactive<FormRules<InfoFormType>>({
+            title: [
+                { required: true, message: '请输入标题', trigger: 'blur' },
+            ],
+            content: [
+                { required: true, message: '请输入资讯内容', trigger: 'blur' }
+            ]
+        })
+        const handleInfoIssue = async (formEl: FormInstance | undefined) => {
+            if (!formEl) return
+            await formEl.validate((valid, fields) => {
+                if (!valid) {
+                    let errorMessage = '';
+                    for (const fieldName in fields) {
+                        if (fields.hasOwnProperty(fieldName)) {
+                            const fieldMessages = fields[fieldName];
+                            fieldMessages.forEach(messageObj => {
+                                errorMessage += messageObj.message + '; ';
+                            });
+                        }
+                    }
+                    ElMessageBox.alert(errorMessage, '注意', {
+                        confirmButtonText: '好的',
+                    })
+                }
+            })
+        }
+
+        // 素材表单
+        const materiaFormRef = ref<FormInstance>()
+        const materiaForm = reactive<MateriaFormType>({
+            title: '',
+            author: '',
+            date: '',
+            radio: '',
+            content: '',
+        })
+        const materiaFormRules = reactive<FormRules<MateriaFormType>>({
+            title: [
+                { required: true, message: '请输入标题', trigger: 'blur' },
+            ],
+            radio: [
+                { required: true, message: '请选择类型', trigger: 'blur' },
+            ],
+            content: [
+                { required: true, message: '请输入资讯内容', trigger: 'blur' }
+            ]
+        })
+        const handleMateriaIssue = async (formEl: FormInstance | undefined) => {
+            if (!formEl) return
+            await formEl.validate((valid, fields) => {
+                if (!valid) {
+                    let errorMessage = '';
+                    for (const fieldName in fields) {
+                        if (fields.hasOwnProperty(fieldName)) {
+                            const fieldMessages = fields[fieldName];
+                            fieldMessages.forEach(messageObj => {
+                                errorMessage += messageObj.message + '; ';
+                            });
+                        }
+                    }
+                    ElMessageBox.alert(errorMessage, '注意', {
+                        confirmButtonText: '好的',
+                    })
+                }
+            })
+        }
+
+        return {
+            menuItemList,
+            itemIndex,
+            handleIndex,
+            infoFormRef,
+            infoForm,
+            infoFormRules,
+            handleInfoIssue,
+            materiaFormRef,
+            materiaForm,
+            materiaFormRules,
+            handleMateriaIssue,
+        }
+    }
 })
 
 </script>
 
 <template>
-    <h1>Issue Page</h1>
+    <div class="page-contain">
+        <menu-item :menuItemList="menuItemList" @item-selected="handleIndex" />
+        <template v-if="itemIndex == 0">
+            <el-form
+                ref="infoFormRef"
+                :model="infoForm"
+                :rules="infoFormRules"
+                class="form"
+            >
+                <el-form-item label="资讯标题" prop="title">
+                    <el-input v-model="infoForm.title" class="input" />
+                </el-form-item>
+                <el-form-item label="资讯内容" prop="content">
+                    <el-input
+                        v-model="infoForm.content"
+                        class="info-content"
+                        :rows="22"
+                        maxlength="1000"
+                        show-word-limit
+                        type="textarea"
+                    />
+                </el-form-item>
+                <el-form-item>
+                    <el-button @click="handleInfoIssue(infoFormRef)" type="primary" class="button">发布</el-button>
+                </el-form-item>
+            </el-form>
+        </template>
+        <template v-else>
+            <el-form
+                ref="materiaFormRef"
+                :model="materiaForm"
+                :rules="materiaFormRules"
+                class="form"
+            >
+                <el-form-item label="素材标题" prop="title">
+                    <el-input v-model="materiaForm.title" class="input" />
+                </el-form-item>
+                <el-form-item label="素材类型" prop="radio">
+                    <el-radio-group v-model="materiaForm.radio">
+                        <el-radio :value='0'>图像</el-radio>
+                        <el-radio :value='1'>视频</el-radio>
+                        <el-radio :value='2'>音频</el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="素材内容" prop="content">
+                    <el-upload
+                        v-model="materiaForm.content"
+                        class="upload-demo"
+                        action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                        multiple
+                        :limit="1"
+                    >
+                        <el-button type="primary">点击上传</el-button>
+                    </el-upload>
+                </el-form-item>
+                <el-form-item>
+                    <el-button @click="handleMateriaIssue(materiaFormRef)" type="primary" class="button">发布</el-button>
+                </el-form-item>
+            </el-form>
+        </template>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+</style>
