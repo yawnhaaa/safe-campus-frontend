188b5fc25a2fd7dd487c55bf7546ea03c406d3cf
yawnhaaa <yawnhaaa@163.com>
2024-03-23 16:04:31 +0800

    评论结束

--- src/components/CommentItem.vue
+++ src/components/CommentItem.vue
@@ -1,13 +1,8 @@
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, reactive, ref } from 'vue'
 import { request, requestJWT } from '@/api/request'
 import { ElMessageBox } from 'element-plus'
-
-export default defineComponent({
-    name: "CommentItem",
-
-    setup() {
-        type CommentType = {
+type CommentType = {
             id: number;
             comment: string;
             commentId: number;
@@ -17,9 +12,17 @@ export default defineComponent({
             topId: number;
             commentTime: string;
             isDelete: number;
+            childList?: CommentType[];
         }
+
+export default defineComponent({
+    name: "CommentItem",
+
+    setup() {
+
         const comment = ref('')
-        const commentList = ref<CommentType[]>([]);
+        const resendComment = ref('')
+        const topCommentList = ref<CommentType[]>([]);
         const infoId = ref(0)
         const handleSend = () => {
             if (comment.value === '') {
@@ -34,17 +37,39 @@ export default defineComponent({
                 commentName: localStorage.getItem("user"),
             }
             requestJWT.post("/sendComment", body).then((res) => {
-                console.log(res)
+                if (res.data.code === 200) {
+                    ElMessageBox.alert("评论成功", "注意", {
+                        confirmButtonText: "好的"
+                    })
+                    return 
+                } else {
+                    ElMessageBox.alert("网络错误", "注意", {
+                        confirmButtonText: "好的"
+                    })
+                    return 
+                }
             })
         }
 
         const getComment = () => {
             request.get('/getComment/' + infoId.value).then((res) => {
-                if(res.data.code === 200) {
+                if (res.data.code === 200 && res.data.data) {
                     const newComments = res.data.data;
-                    newComments.sort((a: CommentType, b: CommentType) => new Date(b.commentTime).getTime() - new Date(a.commentTime).getTime());
-                    commentList.value = newComments
-                    console.log(commentList.value)
+                    const filteredComments = newComments.filter((comment: CommentType) => comment.topId === null);
+                    filteredComments.sort((a: CommentType, b: CommentType) => new Date(b.commentTime).getTime() - new Date(a.commentTime).getTime());
+                    // 遍历原始评论数组，将符合条件的评论添加至父评论的childList中
+                    newComments.forEach((comment: CommentType) => {
+                        if (comment.topId !== null) {
+                            const parentComment = filteredComments.find((parent: CommentType) => parent.id === comment.topId);
+                            if (parentComment) {
+                                if (!parentComment.childList) {
+                                    parentComment.childList = [];
+                                }
+                                parentComment.childList.unshift(comment);
+                            }
+                        }
+                    });
+                    topCommentList.value = filteredComments
                 }
             })
         }
@@ -66,7 +91,53 @@ export default defineComponent({
             const parts = url.split("/")
             return parseInt(parts[parts.length - 1], 10);
         }
-        
+        const commented: CommentType = reactive({
+            id: 0,
+            comment: '',
+            commentId: 0,
+            commentName: '',
+            commentedId: 0,
+            commentedName: '',
+            topId: 0,
+            commentTime: '',
+            isDelete: 0,
+        });
+        const dialogFormVisible = ref(false)
+        const resend = (item: CommentType) => {
+            dialogFormVisible.value = true
+            commented.commentedId = item.commentId
+            commented.commentedName = item.commentName
+            commented.topId = item.topId || item.id
+        }
+        const sendCommented = () => {
+            const body = {
+                infoId: infoId.value,
+                comment: resendComment.value,
+                commentId: localStorage.getItem("userId"),
+                commentName: localStorage.getItem("user"),
+                commentedId: commented.commentedId,
+                commentedName: commented.commentedName,
+                topId: commented.topId
+            }
+            requestJWT.post("/sendComment", body).then((res) => {
+                if (res.data.code === 200) {
+                    ElMessageBox.alert("评论成功", "注意", {
+                        confirmButtonText: "好的"
+                    }).then(() => {
+                        dialogFormVisible.value = false
+                    })
+                    return
+                } else {
+                    ElMessageBox.alert("网络错误", "注意", {
+                        confirmButtonText: "好的"
+                    }).then(() => {
+                        dialogFormVisible.value = false
+                    })
+                    return
+                }
+            })
+        }
+
         onMounted(() => {
             infoId.value = getInfoId()
             getComment()
@@ -74,9 +145,13 @@ export default defineComponent({
 
         return {
             comment,
-            commentList,
+            topCommentList,
             handleSend,
             formatDateTime,
+            dialogFormVisible,
+            resend,
+            resendComment,
+            sendCommented,
         }
     }
 })
@@ -91,15 +166,37 @@ export default defineComponent({
             </div>
             <el-button type="primary" class="send" @click="handleSend">发送</el-button>
         </div>
-        <div class="comment-text" v-for="(item, index) in commentList" :Key="index">
-            <div class="comment-name">{{ item.commentName }}</div>
+        <div class="comment-text" v-for="(item, index) in topCommentList" :Key="index">
+            <div class="comment-name" style="color: gray;">{{ item.commentName }}</div>
             <div class="comment-content">{{ item.comment }}</div>
             <div class="comment-footer">
-                <span class="comment-time">{{ formatDateTime(item.commentTime) }}</span>
-                <span class="comment-resend" @click="resend">回复</span>
+                <span class="comment-time" style="color: gray;">{{ formatDateTime(item.commentTime) }}</span>
+                <span class="comment-resend" @click="resend(item)">回复</span>
             </div>
-
+            <div class="child-comment" v-for="(child, index) in item.childList" :Key="index">
+                <div class="comment-header">
+                    <span>{{ child.commentName }}</span>
+                    <span style="color: gray;">回复</span>
+                    <span>{{ child.commentedName }}</span>
+                </div>
+                <div class="comment-content">{{ child.comment }}</div>
+                <div class="comment-footer">
+                    <span class="comment-time"  style="color: gray;">{{ formatDateTime(child.commentTime) }}</span>
+                    <span class="comment-resend" @click="resend(item)">回复</span>
+                </div>
+            </div>
         </div>
+        <el-dialog v-model="dialogFormVisible" title="回复" top="40vh" width="500">
+            <el-input v-model="resendComment" maxlength="100" class="comment" show-word-limit type="textarea" /> 
+            <template #footer>
+                <div class="dialog-footer">
+                    <el-button @click="dialogFormVisible = false">取消</el-button>
+                    <el-button type="primary" @click="sendCommented">
+                        发送
+                    </el-button>
+                </div>
+            </template>
+        </el-dialog>
     </div>
 </template>
 <style lang="scss" scoped>
@@ -136,7 +233,7 @@ export default defineComponent({
     margin: 20px;
     padding-bottom: 10px;
     border-bottom: 1px solid gray;
-    
+
     .comment-content {
         padding: 10px;
     }
@@ -147,6 +244,16 @@ export default defineComponent({
 
         &:hover {
             cursor: pointer;
+        }
+    }
+}
+
+.child-comment {
+    margin: 10px 0 0 30px;
+
+    .comment-header {
+        span {
+            margin-right: 10px;
         }
     }
 }
