85e9f858937ea564e4e43194808908315ff02995
yawnhaaa <yawnhaaa@163.com>
2024-03-24 20:20:11 +0800

    个人信息功能完成

--- src/pages/MyPage.vue
+++ src/pages/MyPage.vue
@@ -3,6 +3,7 @@ import { defineComponent, ref, reactive, onMounted, watch } from 'vue';
 import MenuItem from '../components/MenuItem.vue';
 import { FormInstance, FormRules, ElMessageBox } from 'element-plus';
 import { useElementPlusTheme } from 'use-element-plus-theme'
+import { requestJWT } from '@/api/request';
 
 export default defineComponent({
     name: 'MyPage',
@@ -37,10 +38,19 @@ export default defineComponent({
         const rules = reactive<FormRules<FormType>>({
             name: [
                 { required: true, message: '请输入昵称', trigger: 'blur' },
-                { min: 2, max: 8, message: '长度在2-8之间', trigger: 'blur' },
+                { min: 2, max: 10, message: '长度在2-8之间', trigger: 'blur' },
             ],
             email: [{ required: true, message: '邮箱不可更改', trigger: 'blur' }],
         })
+        type UpdateFormType = {
+            id: number;  // 根据您的业务逻辑，此处可能是 number 类型，根据需要调整
+            name: string;
+            intro: string;
+            gender: string;  // 假设性别为字符串类型
+            school: string;
+            college: string;
+            stuNum: string;
+        }
         const onSave = async (formEl: FormInstance | undefined) => {
             if (!formEl) return
             await formEl.validate((valid, fields) => {
@@ -56,11 +66,36 @@ export default defineComponent({
                             });
                         }
                     }
-
                     ElMessageBox.alert(errorMessage, '注意', {
                         confirmButtonText: '好的',
                     })
+                    return
                 }
+                const updateForm: UpdateFormType = {
+                    id: Number(localStorage.getItem("userId")) || 0,
+                    name: form.name || '',
+                    intro: form.intro || '',
+                    gender: form.gender || '',
+                    school: form.school || '',
+                    college: form.college || '',
+                    stuNum: form.stunum || ''
+                }
+                requestJWT.post("/updateUserDetail", updateForm).then((res) => {
+                    if (res.data.code === 200) {
+                        ElMessageBox.alert(res.data.data, "注意", {
+                            confirmButtonText: '好的'
+                        }).then(() => {
+                        })
+                    } else {
+                        ElMessageBox.alert(res.data.msg, "注意", {
+                            confirmButtonText: '好的'
+                        })
+                    }
+                }).catch(() => {
+                    ElMessageBox.alert("网络错误", "注意", {
+                        confirmButtonText: '好的'
+                    })
+                })
             })
         }
         // 字体表单
@@ -84,7 +119,23 @@ export default defineComponent({
             document.documentElement.style.setProperty('--color-a-hover', newValue);
         })
 
+        const getUserDetail = () => {
+            requestJWT.get("/getUserDetail/" + localStorage.getItem("userId")).then((res) => {
+                if (res.data.code === 200) {
+                    const userData = res.data.data
+                    form.name = userData.name
+                    form.email = userData.email
+                    form.intro = userData.intro
+                    form.gender = userData.gender
+                    form.school = userData.school
+                    form.college = userData.college
+                    form.stunum = userData.stuNum
+                }
+            })
+        }
+
         onMounted(() => {
+            getUserDetail()
         })
 
         return {
@@ -108,21 +159,16 @@ export default defineComponent({
     <div class="page-contain">
         <menu-item :menuItemList="menuItemList" @item-selected="handleIndex" />
         <template v-if="itemIndex == 0">
-            <el-form 
-                ref="formRef"
-                :model="form" 
-                label-width="auto" 
-                :rules="rules"
-                class="form"
-            >
+            <el-form ref="formRef" :model="form" label-width="auto" :rules="rules" class="form">
                 <el-form-item label="昵称" prop="name">
-                    <el-input v-model="form.name" class="input" />
+                    <el-input v-model="form.name" class="input" maxlength="10" clearable disabled />
                 </el-form-item>
                 <el-form-item label="邮箱" prop="email">
-                    <el-input v-model="form.email" class="input" />
+                    <el-input v-model="form.email" class="input" disabled />
                 </el-form-item>
                 <el-form-item label="个人介绍" prop="intro">
-                    <el-input v-model="form.intro" class="input" />
+                    <el-input v-model="form.intro" class="input" type="textarea" show-word-limit maxlength="200"
+                        clearable />
                 </el-form-item>
                 <el-form-item label="性别" prop="gender">
                     <el-radio-group v-model="form.gender">
@@ -132,13 +178,13 @@ export default defineComponent({
                     </el-radio-group>
                 </el-form-item>
                 <el-form-item label="学校" prop="school">
-                    <el-input v-model="form.school" class="input" />
+                    <el-input v-model="form.school" class="input" maxlength="20" clearable />
                 </el-form-item>
                 <el-form-item label="学院" prop="college">
-                    <el-input v-model="form.college" class="input" />
+                    <el-input v-model="form.college" class="input" maxlength="20" clearable />
                 </el-form-item>
                 <el-form-item label="学号" prop="stunum">
-                    <el-input v-model="form.stunum" class="input" />
+                    <el-input v-model="form.stunum" class="input" maxlength="20" clearable />
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="onSave(formRef)" class="button">保存</el-button>
@@ -146,22 +192,10 @@ export default defineComponent({
             </el-form>
         </template>
         <template v-else>
-            <el-form
-                :model="individForm"
-                class="form"
-            >
+            <el-form :model="individForm" class="form">
                 <el-form-item label="选择字体" prop="font">
-                    <el-select
-                        v-model="individForm.font"
-                        placeholder="选择系统字体"
-                        @change="setFontFamily"
-                    >
-                        <el-option
-                            v-for="font in fontList"
-                            :key="font"
-                            :label="font"
-                            :value="font"
-                        />
+                    <el-select v-model="individForm.font" placeholder="选择系统字体" @change="setFontFamily">
+                        <el-option v-for="font in fontList" :key="font" :label="font" :value="font" />
                     </el-select>
                 </el-form-item>
                 <el-form-item label="选择主题" prop="theme">
@@ -173,5 +207,4 @@ export default defineComponent({
     </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
