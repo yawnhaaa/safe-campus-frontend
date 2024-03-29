197d417f7fda34839e8409e3abc9e6751de1559a
yawnhaaa <yawnhaaa@163.com>
2024-03-29 21:09:28 +0800

    可视化管理底子

---
+++ src/admin-pages/VisualPage.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent ({
+  name: "VisualPage",
+
+  setup() {
+
+    return {
+
+    }
+  }
+})
+</script>
+
+<template>
+  <h1>可视化管理</h1>
+</template>
+
+<style scoped lang="scss">
+
+</style>
