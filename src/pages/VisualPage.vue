989ec452076bdafad4ff60abd5510b2d393e8605
yawnhaaa <yawnhaaa@163.com>
2024-03-31 14:46:54 +0800

    Done

--- src/pages/VisualPage.vue
+++ src/pages/VisualPage.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import {defineComponent, onMounted, ref} from 'vue'
+import {defineComponent, onBeforeUnmount, onMounted, ref} from 'vue'
 import '../styles/page.scss'
 import {request} from "@/api/request";
 import * as echarts from 'echarts'
@@ -17,58 +17,88 @@ export default defineComponent({
   name: "VisualPage",
 
   setup() {
-    const visualList = ref<Visual[]>([])
+    const visualList = ref<Visual[]>([]);
+    let pieChart: echarts.ECharts | null = null;
+    let barChart: echarts.ECharts | null = null;
+    let pie2Chart: echarts.ECharts | null = null;
 
     const getVisualList = () => {
       request.get("/getVisualList").then((res) => {
         if (res.data.code === 200) {
-          visualList.value = res.data.data
+          visualList.value = res.data.data;
         }
-      })
-    }
+      });
+    };
 
     const drawPieChart = () => {
-      const chartDom = document.getElementById('pie-chart') as HTMLDivElement
-      if (!chartDom) return
+      const chartDom = document.getElementById('pie-chart') as HTMLDivElement;
+      if (!chartDom) return;
+      if (pieChart) {
+        pieChart.dispose(); // Dispose previous instance if exists
+      }
+      pieChart = echarts.init(chartDom);
+      pieChart.setOption(pieOption); // 使用上面定义的option配置绘制饼图
+    };
 
-      const myChart = echarts.init(chartDom)
-      myChart.setOption(pieOption) // 使用上面定义的option配置绘制饼图
-    }
     const drawBarChart = () => {
-      const chartDom = document.getElementById('bar-chart') as HTMLDivElement
-      if (!chartDom) return
+      const chartDom = document.getElementById('bar-chart') as HTMLDivElement;
+      if (!chartDom) return;
+      if (barChart) {
+        barChart.dispose(); // Dispose previous instance if exists
+      }
+      barChart = echarts.init(chartDom);
+      barChart.setOption(barOption); // 使用上面定义的option配置绘制饼图
+    };
 
-      const myChart = echarts.init(chartDom)
-      myChart.setOption(barOption) // 使用上面定义的option配置绘制饼图
-    }
     const drawPie2Chart = () => {
-      const chartDom = document.getElementById('pie2-chart') as HTMLDivElement
-      if (!chartDom) return
+      const chartDom = document.getElementById('pie2-chart') as HTMLDivElement;
+      if (!chartDom) return;
+      if (pie2Chart) {
+        pie2Chart.dispose(); // Dispose previous instance if exists
+      }
+      pie2Chart = echarts.init(chartDom);
+      pie2Chart.setOption(pie2Option); // 使用上面定义的option配置绘制饼图
+    };
 
-      const myChart = echarts.init(chartDom)
-      myChart.setOption(pie2Option) // 使用上面定义的option配置绘制饼图
-    }
+    const initChart = () => {
+      drawPieChart();
+      drawPie2Chart();
+      drawBarChart();
+    };
 
     onMounted(() => {
-      getVisualList()
-      drawPieChart()
-      drawPie2Chart()
-      drawBarChart()
-    })
+      getVisualList();
+      initChart();
+    });
+
+    // Ensure to dispose of the charts when the component is unmounted
+    onBeforeUnmount(() => {
+      if (pieChart) {
+        pieChart.dispose();
+      }
+      if (barChart) {
+        barChart.dispose();
+      }
+      if (pie2Chart) {
+        pie2Chart.dispose();
+      }
+    });
 
     return {
       visualList,
-    }
+    };
   }
-})
+});
 
 </script>
 
 <template>
   <div class="page-contain">
-    <div class="data-box" v-for="(item, index) in visualList" :key="index">
-      <div>{{ item.title }}</div>
-      <div>{{ item.number }}</div>
+    <div class="header">
+      <div class="data-box" v-for="(item, index) in visualList" :key="index">
+        <div>{{ item.title }}</div>
+        <div>{{ item.number }}</div>
+      </div>
     </div>
     <div>
       <div class="chart-title">近半年各地区易感人群分布</div>
@@ -86,9 +116,12 @@ export default defineComponent({
 </template>
 
 <style lang="scss" scoped>
+.header {
+  display: flex;
+}
+
 .data-box {
-  display: inline-block;
-  white-space: nowrap;
+  flex: 1;
   padding: 2px;
   margin: 5px 5px 5px 0;
   border: 1px solid gray;
